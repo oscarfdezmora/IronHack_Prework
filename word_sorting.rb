86ca7e051f7ac0971eda84fd57715f49df124ca2
Oscar Fernandez Mora <oscarfdezmora@gmail.com>
2016-09-22 23:58:59 +0200

    Añadido wordsorting

--- word_sorting.rb
+++ word_sorting.rb
@@ -9,19 +9,21 @@
 
 =end
 def word_sorting (phrase)
-
+	#Divide la frase en palabras
 	words = phrase.split
+	#Relación de caracteres que obvia
 	characters = ['¿''?''!''¡'','';''.'':']
 	pairs = []
-
+	#Por cada palabra, crea una dupla de caracteres en minuscula y limpio
+	#Las mayusculas alteran el orden
 	for word in words
 		cleaned = word.gsub(/#{characters}/,'')
 		pairs << [cleaned.downcase, cleaned]
 	end
-
+	#Ordena la lista de duplas según la primera palabra 
 	pairs.sort!
 	final = []
-
+	#Crea una nueva lista con las palabras limpias originales
 	for pair in pairs
 		final << pair[1]
 	end
